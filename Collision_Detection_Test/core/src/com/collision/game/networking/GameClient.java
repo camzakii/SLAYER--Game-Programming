7c2f0b6df689ef5797922c14291cdc6806d8b8c2
VagueLines <vaguelines@hotmail.com>
2015-10-18 00:31:22 +1300

    netwokring

--- Collision_Detection_Test/core/src/com/collision/game/networking/GameClient.java
+++ Collision_Detection_Test/core/src/com/collision/game/networking/GameClient.java
@@ -1,5 +1,22 @@
 package com.collision.game.networking;
 
-public class GameClient {
+import com.collision.game.MyGdxGame;
+import com.collision.game.handler.GameStateManager;
+import com.collision.game.states.GameScreen;
+import com.esotericsoftware.kryonet.Client;
 
+public class GameClient {
+	
+	private Client client;
+	private GameScreen game;
+	
+	private String name;
+	
+	public GameClient(String name){
+		this.game = new GameScreen(new GameStateManager(new MyGdxGame()));
+		this.name = name;
+		
+		this.client = new Client();
+		this.client.start();
+	}
 }
--- Collision_Detection_Test/core/src/com/collision/game/networking/Network.java
+++ Collision_Detection_Test/core/src/com/collision/game/networking/Network.java
@@ -1,5 +1,29 @@
 package com.collision.game.networking;
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryonet.EndPoint;
+
 public class Network {
 
+	public static final int PORT = 6464;
+	public static final int UDP = 6466;
+	public static final int VERSION = 10;
+	
+	public static void register(EndPoint endPoint){
+		Kryo kryo = endPoint.getKryo();
+		kryo.register(Login.class);
+	}
+	
+	
+	public static class Login{
+		public String name;
+		public int version;
+
+		public Login() {}
+		public Login(String name, int version){
+			this.name = name;
+			this.version = version;
+		}
+	}
+	
 }
